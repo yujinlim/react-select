275c0a6b1003064dff5914aa9081f36a00ba40d1
Yu-Jin <adamlyj89@gmail.com>
2015-04-16 16:26:10 +0800

    update value from selected value

--- src/Select.js
+++ src/Select.js
@@ -220,6 +220,7 @@ var Select = React.createClass({
 		this._focusAfterUpdate = true;
 		var newState = this.getStateFromValue(value);
 		newState.isOpen = false;
+		newState.inputValue = value.label; // set label as value to show
 		this.fireChangeEvent(newState);
 		this.setState(newState);
 	},
